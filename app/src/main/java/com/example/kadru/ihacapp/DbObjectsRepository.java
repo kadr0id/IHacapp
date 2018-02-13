a7aea581e2be6bb2985751cd022dec9446fa43cb
kadr0id <kadruk@gmail.com>
2018-02-13 12:27:36 +0200

    add objects.active =1 and objects.wholesaler_id is null

--- app/src/main/java/com/example/kadru/ihacapp/DbObjectsRepository.java
+++ app/src/main/java/com/example/kadru/ihacapp/DbObjectsRepository.java
@@ -18,22 +18,22 @@ public class DbObjectsRepository {
     public ArrayList<String> getData(){
         ArrayList<String> locations = new ArrayList<>();
         if (MainActivity.typeId == 1) {
-            cur = db.rawQuery("select * from objects_locations where location_id = 1;", null);
+            cur = db.rawQuery("select objects.pictogram, object_lang.name from objects_locations inner join object_lang on objects_locations.object_id = object_lang._id inner join objects on  object_lang._id = objects._id where objects_locations.location_id =1 and object_lang.language =1 and objects.active =1 and objects.wholesaler_id is null;", null);
         }
         if (MainActivity.typeId == 18){
-            cur = db.rawQuery("select * from objects_locations where location_id = 18;", null);
+            cur = db.rawQuery("select objects.pictogram, object_lang.name from objects_locations inner join object_lang on objects_locations.object_id = object_lang._id inner join objects on  object_lang._id = objects._id where objects_locations.location_id =18 and object_lang.language =1 and objects.active =1 and objects.wholesaler_id is null;", null);
         }
         if (MainActivity.typeId == 19){
-            cur = db.rawQuery("select * from objects_locations where location_id = 19;", null);
+            cur = db.rawQuery("select objects.pictogram, object_lang.name from objects_locations inner join object_lang on objects_locations.object_id = object_lang._id inner join objects on  object_lang._id = objects._id where objects_locations.location_id =19 and object_lang.language =1 and objects.active =1 and objects.wholesaler_id is null;", null);
         }
         if (MainActivity.typeId == 20){
-            cur = db.rawQuery("select * from objects_locations where location_id = 20;", null);
+            cur = db.rawQuery("select objects.pictogram, object_lang.name from objects_locations inner join object_lang on objects_locations.object_id = object_lang._id inner join objects on  object_lang._id = objects._id where objects_locations.location_id =20 and object_lang.language =1 and objects.active =1 and objects.wholesaler_id is null;", null);
         }
         if (MainActivity.typeId == 21){
-            cur = db.rawQuery("select * from objects_locations where location_id = 21;", null);
+            cur = db.rawQuery("select objects.pictogram, object_lang.name from objects_locations inner join object_lang on objects_locations.object_id = object_lang._id inner join objects on  object_lang._id = objects._id where objects_locations.location_id =21 and object_lang.language =1 and objects.active =1 and objects.wholesaler_id is null;", null);
         }
         if (MainActivity.typeId == 22){
-            cur = db.rawQuery("select * from objects_locations where location_id = 22;", null);
+            cur = db.rawQuery("select objects.pictogram, object_lang.name from objects_locations inner join object_lang on objects_locations.object_id = object_lang._id inner join objects on  object_lang._id = objects._id where objects_locations.location_id =22 and object_lang.language =1 and objects.active =1 and objects.wholesaler_id is null;", null);
         }
         if (cur != null && cur.moveToFirst()) {
             do {
